77ad7572df4ed672f97f58102c118a56cc75e94a
bugsfounder <bugsfounder2021@gmail.com>
2025-04-01 09:17:09 +0530

    writing first program

---
+++ FirstJavaProgram/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+// add two numbers
+// AddTwoNumbers --> Pascal case naming convention
+// addTwoNumbers --> Camel case naming convention
