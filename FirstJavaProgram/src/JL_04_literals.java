d8183d93bcc72aee664bd010ca152e5a7398a579
bugsfounder <bugsfounder2021@gmail.com>
2025-04-03 16:50:28 +0530

    variables and datatypes

---
+++ FirstJavaProgram/src/JL_04_literals.java
@@ -0,0 +1,17 @@
+public class JL_04_literals {
+    public static void main(String[] args) {
+        byte age = 34;
+        int age2 = 23;
+        short age3 = 43;
+        long ageDino = 566666666666666666L;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 4.66d;
+        boolean a = true;
+
+        System.out.println(age);
+        
+        String str  ="Manisha";
+        System.out.println(str);
+    }
+}
--- FirstJavaProgram/src/Main.java
+++ FirstJavaProgram/src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        int num1 = 12, num2=43, num3=53;
+        int sum = num1+num2+num3;
+        System.out.println("sum of three  numbers: "+ sum);
     }
 }
 
